98ebe73ce4a7a1c5d2f4f9eced631b7b0c143a4e
Nattawat Nattachanasit <6531503201@lamduan.mfu.ac.th>
2025-01-31 14:59:48 +0700

    refactor: update nuxt.config.ts to use Vercel preset and adjust vercel.json rewrites

--- apps/nuxt-landing-arch/nuxt.config.ts
+++ apps/nuxt-landing-arch/nuxt.config.ts
@@ -16,11 +16,6 @@ export default defineNuxtConfig({
     },
   },
   nitro: {
-    storage: {
-      data: {
-        driver: 'vercelKV'
-        /* Vercel KV driver options */
-      }
-    }
+    preset: "vercel"
   }
 });
